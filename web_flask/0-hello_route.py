790be29ce6da7b3c573bc3b85d86f9a67a1399d9
blsoko <exanymejose1@hotmail.com>
2021-01-24 15:14:42 -0500

    first attempt flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Creating my first flask program
+from flask import Flask
+
+app = Flask(__name__)
+strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    # Display hello hbnb at host='0.0.0.0', port=5000
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
