f1b2e367e05067dab78a2efb5d0bac005fe5ebb6
blsoko <exanymejose1@hotmail.com>
2021-01-24 16:11:18 -0500

    first attempt

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" Creating my first flask program
+"""
+from flask import Flask
+
+app = Flask(__name__)
+strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """ Display hello hbnb at host='0.0.0.0', port=5000
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hellohbtn():
+    """ Display HBTN
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def displayText(text):
+    """ display “C ” followed by the value
+    """
+    return ("C %s" % text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def pythonText(text):
+    """ display “Python ” followed by the value
+    """
+    return ("Python %s" % text.replace('_', ' '))
+
+
+@app.route('/number/<n>')
+def displayNumber(n):
+    """ display “C ” followed by the value
+    """
+    if n.isnumeric():
+        return ("%s is a number" % n)
+    return
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
