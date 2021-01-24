529832fd22aad7ddefe2056758d9da659a882e28
blsoko <exanymejose1@hotmail.com>
2021-01-24 15:17:43 -0500

    correct python doc

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# Creating my first flask program
+""" Creating my first flask program
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,7 +9,8 @@ strict_slashes = False
 
 @app.route('/')
 def hello():
-    # Display hello hbnb at host='0.0.0.0', port=5000
+    """ Display hello hbnb at host='0.0.0.0', port=5000
+    """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
